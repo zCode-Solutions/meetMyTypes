048745cfac88ea2f279484c4b564cadb61a39b52
analogy47 <actweed47@gmail.com>
2020-01-04 20:22:30 -0500

    Add standard as dev dependency

--- meetmytypesadmin/src/App.js
+++ meetmytypesadmin/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Login from "./components/Login";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Login from './components/Login'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
