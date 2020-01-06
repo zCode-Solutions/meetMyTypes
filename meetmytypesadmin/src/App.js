e0b854c40a0e853b674ec6ba67939901775ca4e6
Mohamed Eshaftri <eshaftri@gmail.com>
2020-01-06 00:51:56 +0000

    sidebar container completed

--- meetmytypesadmin/src/App.js
+++ meetmytypesadmin/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./containers/Sidebar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <React.Fragment>
-    <h3>Admin Home</h3>
+      <Sidebar/>
     </React.Fragment>
   );
 }
---
+++ meetmytypesadmin/src/containers/Sidebar/index.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+
+import logo from '../../logo/meetmytypes-logo.png';
+import "./style.css";
+
+export default () => {
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <div className="sidenav">
+        <Link className="btnn" to="/">
+        <img alt = "logo" src={logo} width="100" height="100" />
+        </Link>
+        <Link  to="/new-user">NEW USER</Link>
+        <Link  to="/current-users">CURRENT USERS</Link>
+        <Link  to="/search">SEARCH</Link>
+        <Link  to="/profile">PROFILE</Link>
+        </div>
+      </Fragment>
+    </BrowserRouter>
+  );
+};
