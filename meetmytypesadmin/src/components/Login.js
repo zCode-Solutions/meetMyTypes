4a78fe2db4f4f9e14291e739279a5eaa20c103bc
analogy47 <actweed47@gmail.com>
2020-01-08 01:57:50 -0500

    Add functionality for cognito login

--- meetmytypesadmin/src/components/Login.js
+++ meetmytypesadmin/src/components/Login.js
@@ -1,44 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
 
-export default function Login () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   // might want better validation with messages
   const validateForm = () => {
-    return email.length > 0 && password.length > 0
-  }
+    return email.length > 0 && password.length > 0;
+  };
 
-  const handleSubmit = event => {
-    // just logging to console for now
-    event.preventDefault()
-    console.log(email, password)
-  }
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      await Auth.signIn(email, password);
+      alert('logged in');
+      // redirect user to /admin
+    } catch (err) {
+      alert(err.message);
+    }
+  };
   return (
-    <div className='login'>
+    <div className="login">
       <form onSubmit={handleSubmit}>
-        <label htmlFor='email'>Email</label>
+        <label htmlFor="email">Email</label>
         <input
           autoFocus
-          name='email'
-          type='text'
+          name="email"
+          type="text"
           value={email}
-          placeholder='Enter your email'
+          placeholder="Enter your email"
           onChange={event => setEmail(event.target.value)}
         />
 
-        <label htmlFor='email'>Password</label>
+        <label htmlFor="email">Password</label>
         <input
-          name='password'
-          type='password'
+          name="password"
+          type="password"
           value={password}
-          placeholder='Enter your password'
+          placeholder="Enter your password"
           onChange={event => setPassword(event.target.value)}
         />
-        <button disabled={!validateForm()} type='submit'>
+        <button disabled={!validateForm()} type="submit">
           Login
         </button>
       </form>
     </div>
-  )
+  );
 }
