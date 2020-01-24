a52a6929fe5fe64669d1c367cff6eefb027cb9d6
Jason Brunelle <jason@jasonbrunelle.com>
2020-01-24 13:50:02 -0600

    Add EditUser and LoveTypesDropDown component

---
+++ meetmytypesadmin/src/containers/EditUser/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import Title from '../../components/Title';
+import LoveTypesDropDown from '../../components/LoveTypesDropDown';
+import spinner from '../Spinner-1s-200px.svg';
+import "./style.css";
+
+export default class extends Component {
+    state = {
+        user: {},
+        userLoading: true,
+        mainLoveType: '',
+        top4LoveTypes: [],
+        personalityProfile: ''
+    }
+    componentDidMount() {
+        fetch(
+            'https://qj87hoxzmk.execute-api.us-east-1.amazonaws.com/Dev/currentUsers/' + this.props.match.params.uid 
+          )
+            .then(res => res.json())
+            .then(data => {
+              this.setState({ user: data });
+              this.setState({mainLoveType: data.LoveType});
+              this.setState({personalityProfile: data.Description});
+              this.setState({top4LoveTypes: data.Top4LoveTypes});
+              this.setState({ userLoading: false });
+            })
+            .catch(err => console.log(err));
+        setTimeout(() => console.log(this.state), 3000);
+    }
+
+    onSelectMainLoveTypeHandler = type => {
+        this.setState({mainLoveType: type[0]});
+    }
+
+    onSelectTop4LoveTypeHandler = types => {
+        this.setState({top4LoveTypes: types});
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        // console.log(e.target.value);
+        // console.dir(e.target);
+        const userInfo = {
+            Gender: this.state.user.Gender,
+            Top4LoveTypes: this.state.top4LoveTypes,
+            LoveType: this.state.mainLoveType,
+            Nickname: this.state.user.Nickname,
+            Description: this.state.personalityProfile,
+            email: this.state.user.email,
+            UID: this.props.match.params.uid,
+            Age: this.state.user.Age
+        }
+        console.dir(userInfo);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <div className="main">
+                    <Title
+                        title="Edit User"
+                        detail="Below you can edit a user's profile information"
+                    />
+                    {this.state.userLoading ? (
+                        <div className="loading"><img src={spinner} alt="loading" /></div>
+                    ) : (
+                    <div className="edit-user">
+                        <div><span>Name:</span><span>{this.state.user.Nickname}</span></div>
+                        <div><span>Love Type:</span><span><LoveTypesDropDown onSelectLoveType={this.onSelectMainLoveTypeHandler} selectedTypes={[this.state.mainLoveType]} multiple={false} /></span></div>
+                        <div><span>Top 4 Love Types:</span><span><LoveTypesDropDown onSelectLoveType={this.onSelectTop4LoveTypeHandler} selectedTypes={this.state.top4LoveTypes} multiple={true} /></span></div>
+                        <div><span>Personality Profile:</span><span><textarea value={this.state.personalityProfile} onChange={(e) => {this.setState({personalityProfile: e.target.value})}} onKeyUp={(e) => {this.setState({personalityProfile: e.target.value})}} /></span></div>
+                    </div>)}
+                    <div className="input-group">
+                        <button
+                            type="submit"
+                            className="input-group-text bg-primary text-white m-4"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            className="input-group-text bg-primary text-white m-4"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
